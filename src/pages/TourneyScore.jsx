e28ddd2db27a89f73381879f55500ed43fac97a5
the13thgeek <aldreich.aguilar@yahoo.com>
2026-05-23 12:04:07 -0400

    Scoreboard update

--- src/pages/TourneyScore.jsx
+++ src/pages/TourneyScore.jsx
@@ -3,13 +3,13 @@ import './TourneyScore.scss';
 
 const TourneyScore = () => {
   const [scoreBoard, setScoreBoard] = useState([]);
-  const [effects, setEffects] = useState(null);
+  //const [effects, setEffects] = useState(null);
 
   useEffect(() => {
       const fetchScores = async()  => {  
         try {
-          const response = await fetch(`https://the13thgeek-nodejs.fly.dev/mainframe/showdown-scores`,
-          //const response = await fetch(`http://localhost:8080/mainframe/showdown-scores`,
+          //const response = await fetch(`https://the13thgeek-nodejs.fly.dev/mainframe/showdown-scores`,
+          const response = await fetch(`http://localhost:8080/tourney/scores`,
             {
               method: 'POST',
               headers: {
@@ -19,9 +19,10 @@ const TourneyScore = () => {
             });
           if(response) {
             const result = await response.json();
-            
-            setScoreBoard(result.scoreboard);
-            setEffects(result.effects);
+            console.log('Fetched Scores: ', result.data.scores);
+
+            setScoreBoard(result.data.scores);
+            //setEffects(result.effects);
           }
         } catch(e) {
           console.log('[Scores] Error: ' + e.message);
@@ -31,7 +32,8 @@ const TourneyScore = () => {
       fetchScores();
   
       // Real-time updater
-      const ws = new WebSocket("wss:///the13thgeek-nodejs.fly.dev");
+      //const ws = new WebSocket("wss:///the13thgeek-nodejs.fly.dev");
+      const ws = new WebSocket("ws://localhost:8080");
       ws.onmessage = (event) => {
         const data = JSON.parse(event.data);
 
@@ -45,27 +47,27 @@ const TourneyScore = () => {
   return (
     <>
     <div className='tscore'>
-      {scoreBoard && scoreBoard.length >= 3 && (
+      {scoreBoard && scoreBoard.length >= 0 && (
         <>
-        <div className={`team afb ${effects.blocked_teams.some(([team, reason]) => team === scoreBoard[0].team_number) ? 'grounded' : ''}`}>
+        <div className={`team afb`}>
           <div className="name">Afterburner</div>
-          <div className="score">{scoreBoard[0].total_points}</div>
+          <div className="score">{scoreBoard[0] ? scoreBoard[0].total_points : 0}</div>
         </div>
-        <div className={`team ccd ${effects.blocked_teams.some(([team, reason]) => team === scoreBoard[1].team_number) ? 'grounded' : ''}`}>
+        <div className={`team ccd`}>
           <div className="name">Concorde</div>
-          <div className="score">{scoreBoard[1].total_points}</div>
+          <div className="score">{scoreBoard[1] ? scoreBoard[1].total_points : 0}</div>
         </div>
-        <div className={`team sts ${effects.blocked_teams.some(([team, reason]) => team === scoreBoard[2].team_number) ? 'grounded' : ''}`}>
+        <div className={`team sts`}>
           <div className="name">Stratos</div>
-          <div className="score">{scoreBoard[2].total_points}</div>
+          <div className="score">{scoreBoard[2] ? scoreBoard[2].total_points : 0}</div>
         </div>
         </>
       )}
       
     </div>
-    <pre>
-      {JSON.stringify(effects, null, 2)}
-    </pre>
+    {/* <pre>
+      {JSON.stringify(scoreBoard, null, 2)}
+    </pre> */}
     </>
   )
 }
